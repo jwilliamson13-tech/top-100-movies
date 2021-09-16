e8c5dc54a186e08e8a8fc502a8e7b32c35707248
jwilliamson13-tech <joshua@joshuawilliamson.net>
2021-09-16 07:54:46 -0500

    Working on dashboard
    Finish dashboard design and potentially add search bar (redirects to /movies?query
    
    After this, work on user profile.

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -4,19 +4,30 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Dashboard = props => {
 
-  const { user, isAuthenticated, isLoading } = useAuth0();
-
-  if(isLoading){
-    return(<div>LOADING ...</div>)
-  }
+const bgImageStyle = {
+  backgroundImage: 'url(\'\https://images.unsplash.com/photo-1592780828756-c418d71faa1f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80\')',
+  height: "40vh"
+}
 
   return (
-    isAuthenticated && (
-      <div>
-        <h1>DASHBOARD GOES HERE</h1>
-        <img src={user.picture}/>
+    <div className="contianer-fluid rounded">
+      <div className="container  bg-image pt-5" style={bgImageStyle}>
+        <h1 className=" text-light rounded">Welcome to Top 100 Movies</h1>
+        <h3 className="text-light pb-5 pt-3">This is a place where you can rank your top 100 movies</h3>
+        <h3 className="text-light">SEARCH BAR GOES HERE?</h3>
       </div>
-    )
+      <div className="row mt-5 mb-3 rounded justify-content-center align-items-center">
+        <div className="col-md-6 text-center">
+          <p>Already have an account?</p>
+          <Link className="btn btn-primary">Login</Link>
+        </div>
+        <div className="col-md-6 text-center">
+          <p>New here?</p>
+          <Link className="btn btn-primary">Register</Link>
+        </div>
+      </div>
+
+    </div>
   );
 };
 
